a2fc53f0668149279348c658c37cf23b6bb4d3b1
Allan Young <ayounggtr@gmail.com>
2023-11-28 07:23:57 -0500

    Palindrome

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalendrome(string word)
+        {
+            if (word == null || word.Length == 0)
+            {
+                return false;
+            }
+
+            string wordLowered = word.ToLower();
+
+            string front = "";
+            string back = "";
+
+            for (int i = wordLowered.Length - 1; i >= 0; i--)
+            {
+                back += wordLowered[i];
+            }
+            if(wordLowered == back)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,31 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("radar", true)]
+        [InlineData("defined", false)]
+        [InlineData("abacab", false)]
+        [InlineData("detartrated", true)]
+        [InlineData("bob", true)]
+        [InlineData("noon", true)]
+        [InlineData("sees", true)]
+        [InlineData("racecar", true)]
+        [InlineData("Racecar", true)]
+        [InlineData("", false)]
+        [InlineData(null, false)]
 
+        public void IsAPalindrome_Test(string word, bool expected)
+        {
+            var ws = new WordSmith();
+
+            var instance = ws.IsAPalendrome(word);
+
+            Assert.Equal(expected, instance);
         }
     }
 }
